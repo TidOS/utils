3b6d4fcfd89ed286b3f42df8ad727d3e4c35fcd8
Jordan Thomas <jordanmthomas@gmail.com>
2021-03-18 15:27:07 -0500

    fix dtopbot

--- dtopbot.sh
+++ dtopbot.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
-SLEEPTIME=600
+SLEEPTIME=300
 DTOPFINDERSCRIPT="/home/jordan/src/utils/find-dtops.py"
 POSTEDTOPSFILE="/home/jordan/.dtopbotthreads"
 THREAD=`eval $DTOPFINDERSCRIPT`
 SCROT="/home/jordan/.desktop.png"
 SCROTPOST="/home/jordan/.post.png"
 POSTERSCRIPT="/home/jordan/src/DesktopThread/DesktopThread.py"
-COMMENT=`misfortune`
+COMMENT=`misfortune | sed 's/\-//g'`
 TIDUSPATH="/home/jordan/Nextcloud/Photos/Tidus/Tidus.jpg"
 cd ~/src/DesktopThread
+echo "desktop bot"
 while true
 do
     if [ ! -f "$POSTEDTOPSFILE" ]; then
         touch $POSTEDTOPSFILE
     fi
 
-    convert $SCROT \
-        \( +clone \
+    if [ "$THREAD" -lt 0 ]; then
+        echo "no desktop threads found"
+    fi
+
+    if grep -Fxq $THREAD $POSTEDTOPSFILE
+        then
+            #clear
+	    POSTING="False"
+            #echo "already posted in "$THREAD
+        else
+	    echo "posting in $THREAD"
+            # not posted in the thread yet
+            echo $THREAD >> $POSTEDTOPSFILE
+            convert $SCROT \
+            \( +clone \
             -fill "#111111" \
             -draw 'color 0,0 reset' \
             -resize x40! \
@@ -29,22 +43,13 @@ do
             -gravity west \
                 \( $TIDUSPATH -resize x35 \) \
             -geometry +2.5+0 -composite \) \
-        -append $SCROTPOST
+            -append $SCROTPOST
 
-    if [ "$THREAD" -lt 0 ]; then
-        echo "no desktop threads found"
+            $POSTERSCRIPT -G -X -F $SCROTPOST -C "$COMMENT"
 
-    fi
-
-    if grep -Fxq $THREAD $POSTEDTOPSFILE
-        then
-            echo "already posted in "$THREAD
-        else
-            # not posted in the thread yet
-            echo $THREAD >> $POSTEDTOPSFILE
-            $POSTERSCRIPT -G -X -N "Anonymous" -F $SCROTPOST -C "$COMMENT"
     fi
 sleep $SLEEPTIME
+THREAD=`eval $DTOPFINDERSCRIPT`
 done
 
 
